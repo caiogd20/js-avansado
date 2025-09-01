08346c58981e236f879ce6954102ff3d0a40b7bd
Caio Gonçalves Dias <caiogd20@gmail.com>
2025-09-01 17:13:47 -0300

    Desestruturação

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-let out = document.getElementById('out')
-out.innerHTML = ''
-let a;
-try {
-    a = x;
-} catch (error) {
-    console.error(error.name)
-    console.error(error.message)
-    console.error(error.stack)
-    out.innerHTML = `<div class="alert alert-danger" role="alert">Valor de A não pode ser calculado<br/>${error.message}</div>`
-}
-console.log('fim')
+let nomeComleto = 'josé Colves';
+let [prenome, sobrenome] = nomeComleto.split(' ');
+
+console.log(prenome);   // josé
+console.log(sobrenome); // Colves
