9301a6c653c66b46a2ac65664574a81f7aa133cb
Caio Gonçalves Dias <caiogd20@gmail.com>
2025-09-02 15:14:43 -0300

    Closure3

--- script.js
+++ script.js
@@ -1,33 +1,22 @@
 let out = document.getElementById('out');
-let ola = digaOla('pt-br'); // valor padrão
-
-function digaOla(lg){
-    return function(nome){
-        switch (lg) {
-            case 'es':
-                out.innerHTML = `Hola ${nome}`;
-                break;
-            case 'en':
-                out.innerHTML = `Hello ${nome}`;
-                break;
-            case 'fr':
-                out.innerHTML = `Bonjour ${nome}`;
-                break;
-            default:
-                out.innerHTML = `Ola ${nome}`;
-                break;
-        }
-    }    
+function criaContador(){
+    let n = 0;
+    function altera(v){
+        n += v;
+    };
+    return {
+        inc: function(){ altera(1); },
+        dec: function(){ altera(-1); },
+        valor: function(){ return n; }
+    };
 }
-
-addEventListener('change', e => {
-    if (e.target.name === 'idioma') {
-        ola = digaOla(e.target.value);
-    }
-})
-addEventListener('click', e => {
-    if (e.target.id === 'btn') {
-        let nome = document.getElementById('nome').value;
-        ola(nome);
-    }
-})
+let contador = criaContador();
+out.innerText = contador.valor();
+function inc(){
+    contador.inc();
+    out.innerText = contador.valor();
+}
+function dec(){
+    contador.dec();
+    out.innerText = contador.valor();
+}
