d2eaee808512b8f31760dce425792c0932059111
Caio Gonçalves Dias <caiogd20@gmail.com>
2025-09-02 16:23:35 -0300

    prototipo

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let out = document.getElementById('out');
-let a = 1;
-const b = 2;
-var c = 3;
-this.d = 4; 
-out.innerText = this;
-console.log(this);
+function Foma(x,y){
+    this.x = x;
+    this.y = y;
+}
+let f1 = new Foma(10,20);
+console.log(f1);
+
+
+
+Foma.prototype.move=function(x,y){
+    this.x += x;
+    this.y += y;
+}
+f1.move(100,200);
+console.log(f1);
