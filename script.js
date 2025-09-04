c61b3bb92ee5c09346696b2257d5787df0b0f88f
Caio Gonçalves Dias <caiogd20@gmail.com>
2025-09-04 08:43:17 -0300

    Async

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-let out = document.getElementById('out');
-function Foma(x,y){
-    this.x = x;
-    this.y = y;
+let container = document.getElementById('container');
+let Pgeo = document.createElement('p');
+Pgeo.id = 'geo';
+container.appendChild(Pgeo);
+let mostraCoods = function (pos) {
+    console.log(`latitude: ${pos.coords.latitude}`);
+    console.log(`longitude: ${pos.coords.longitude}`);
+    Pgeo.innerText = `latitude: ${pos.coords.latitude}
+    longitude: ${pos.coords.longitude}`;
 }
-let f1 = new Foma(10,20);
-console.log(f1);
-
-
-
-Foma.prototype.move=function(x,y){
-    this.x += x;
-    this.y += y;
+let erroCoods = function (erro) {
+    console.error(`Erro: ${erro.code} - ${erro.message}`);
+    out.innerText = `Erro: ${erro.code} - ${erro.message}`;
 }
-f1.move(100,200);
-console.log(f1);
+let geo = navigator.geolocation;
+geo.getCurrentPosition(mostraCoods, erroCoods);
+let Pdb = document.createElement('p');
+Pdb.id = 'db';
+container.appendChild(Pdb);
+let db;
+let rqsDB = indexedDB.open('meuDB', 1);
+
+rqsDB.onsuccess = function (event) {
+    db = event.target.result;
+    console.log('Banco aberto com sucesso');
+    Pdb.innerText = 'Banco aberto com sucesso';
+}
+rqsDB.onerror = function (event) {
+    console.error('Erro ao abrir o banco', event.target.error);
+    Pdb.innerText = `Erro ao abrir o banco: ${event.target.error}`;
+}
