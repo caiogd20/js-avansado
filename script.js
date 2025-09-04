cfe449775f7f217af536c1b037c56b59c3829466
Caio Gonçalves Dias <caiogd20@gmail.com>
2025-09-04 08:46:47 -0300

    Promise

--- script.js
+++ script.js
@@ -2,30 +2,43 @@ let container = document.getElementById('container');
 let Pgeo = document.createElement('p');
 Pgeo.id = 'geo';
 container.appendChild(Pgeo);
-let mostraCoods = function (pos) {
-    console.log(`latitude: ${pos.coords.latitude}`);
-    console.log(`longitude: ${pos.coords.longitude}`);
-    Pgeo.innerText = `latitude: ${pos.coords.latitude}
-    longitude: ${pos.coords.longitude}`;
+
+function getCurrentPositionPromise() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
 }
-let erroCoods = function (erro) {
-    console.error(`Erro: ${erro.code} - ${erro.message}`);
-    out.innerText = `Erro: ${erro.code} - ${erro.message}`;
-}
-let geo = navigator.geolocation;
-geo.getCurrentPosition(mostraCoods, erroCoods);
+
+getCurrentPositionPromise()
+    .then(pos => {
+        console.log(`latitude: ${pos.coords.latitude}`);
+        console.log(`longitude: ${pos.coords.longitude}`);
+        Pgeo.innerText = `latitude: ${pos.coords.latitude}
+        longitude: ${pos.coords.longitude}`;
+    })
+    .catch(erro => {
+        console.error(`Erro: ${erro.code} - ${erro.message}`);
+        Pgeo.innerText = `Erro: ${erro.code} - ${erro.message}`;
+    });
+
 let Pdb = document.createElement('p');
 Pdb.id = 'db';
 container.appendChild(Pdb);
-let db;
-let rqsDB = indexedDB.open('meuDB', 1);
 
-rqsDB.onsuccess = function (event) {
-    db = event.target.result;
-    console.log('Banco aberto com sucesso');
-    Pdb.innerText = 'Banco aberto com sucesso';
+function openIndexedDBPromise(name, version) {
+    return new Promise((resolve, reject) => {
+        let request = indexedDB.open(name, version);
+        request.onsuccess = event => resolve(event.target.result);
+        request.onerror = event => reject(event.target.error);
+    });
 }
-rqsDB.onerror = function (event) {
-    console.error('Erro ao abrir o banco', event.target.error);
-    Pdb.innerText = `Erro ao abrir o banco: ${event.target.error}`;
-}
+
+openIndexedDBPromise('meuDB', 1)
+    .then(db => {
+        console.log('Banco aberto com sucesso');
+        Pdb.innerText = 'Banco aberto com sucesso';
+    })
+    .catch(error => {
+        console.error('Erro ao abrir o banco', error);
+        Pdb.innerText = `Erro ao abrir o banco: ${error}`;
+    });
