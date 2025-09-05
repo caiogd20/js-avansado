9ed3ed2165444657f97b963e261c19426925623b
Caio Gonçalves Dias <caiogd20@gmail.com>
2025-09-05 10:55:53 -0300

    Async-Await

--- script.js
+++ script.js
@@ -1,15 +1,56 @@
 let container = document.getElementById('container');
-function carga(n) {
-    fetch('https://reqres.in/api/users/'+n)
-        .then((resp) => resp.json())
-        .then((obj) => console.log(obj))
-        .catch(err => {
-            let out = document.getElementById('users');
-            console.error(err);
-            out.innerHTML = 'Erro ao carregar usuários: ' + err;
-        });
+container.innerHTML = '<input type="number" id="in" value="0"> <input type="button" id="submit" value="submit" class="btn btn-primary" onclick="submit()"><br>';
+
+let input = document.getElementById('in');
+
+// Função auxiliar para aguardar um tempo
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
-container.innerHTML = '<button onclick="carga(1)">Carregar Usuário 1</button><button onclick="carga(2)">Carregar Usuário 2</button><div id="users"></div>';
 
+// Agora fnmetade é async
+async function fnmetade(n) {
+    let num = Number(n);
+    let ms = Math.floor(Math.random() * 2000) + 500;
+    await delay(ms);
+    if (num % 2 == 0) {
+        return num / 2;
+    } else {
+        throw 'Valor não é par';
+    }
+}
 
-carga(1);
+let Pcaunt = 0;
+let PcauntF = 0;
+
+function novoP() {
+    let p = document.createElement('p');
+    p.setAttribute('id', 'p' + Pcaunt);
+    p.innerHTML = 'continua sem esperar o resultado da requisição de id: ' + Pcaunt;
+    container.appendChild(p);
+    Pcaunt++;
+}
+
+function atualizaP(text, sucesso) {
+    let p = document.getElementById('p' + PcauntF);
+    if (!sucesso) p.style.color = 'red';
+    if (sucesso) p.style.color = 'green';
+    p.innerHTML = 'Resultado da requisição de id ' + PcauntF + ': ' + text;
+    PcauntF++;
+}
+
+// submit pode ser normal, mas precisa usar then/catch com async fnmetade
+function submit() {
+    let n = input.value;
+    fnmetade(n)
+        .then(resp => {
+            console.log(resp);
+            atualizaP(`a merade de ${n} é ${resp}`, true);
+        })
+        .catch(err => {
+            console.log(err);
+            atualizaP(err, false);
+        });
+    novoP();
+    console.log('continua sem esperar o resultado');
+}
